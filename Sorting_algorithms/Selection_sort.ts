0121c1099d967422cd3a766cd684c92af3e56399
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 08:06:10 +0300

    Create Selection_sort.ts

---
+++ Sorting_algorithms/Selection_sort.ts
@@ -0,0 +1,14 @@
+function selection_sort(arr: number[]): void {
+    const n: number = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex: number = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+}
